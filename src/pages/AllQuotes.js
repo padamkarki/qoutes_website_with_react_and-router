c3865be414e0975d48b64c86c987461f2689fca4
Padam Karki <padamkarkipkkp@gmail.com>
2022-08-22 16:25:49 +0530

    added not found, quote form, mainNav and updates

--- src/pages/AllQuotes.js
+++ src/pages/AllQuotes.js
@@ -1,5 +1,12 @@
+import QuoteList from "../components/quotes/QuoteList";
+
+const DUMMY_QUOTES = [
+  { id: "p1", authot: "Padam", text: "Learning React is fun" },
+  { id: "p2", authot: "Padam Karki", text: "Learning React is Awesome" },
+];
+
 const AllQuotes = () => {
-  return <h1>All Quotes Page</h1>;
+  return <QuoteList quotes={DUMMY_QUOTES} />;
 };
 
 export default AllQuotes;
